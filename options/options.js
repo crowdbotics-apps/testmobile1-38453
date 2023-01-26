c11247862319477834229801e7f51402ed32bc97
Crowdbotics <engineering@crowdbotics.com>
2023-01-26 01:35:24 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,11 @@ export const globalOptions = {
   name: "testmobile1-38453",
   url: "https://testmobile1-38453.botics.co",
   api: "https://testmobile1-38453.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
-  }
-};
+  },
+  undefined: { styles: styles }
+}
